f9c9d57d5b9454b89bdb672765cb4f718733f0bf
andywarthol <andywarthol@gmail.com>
2015-02-27 19:04:17 -0800

    press page spacing issue

--- press.php
+++ press.php
@@ -45,47 +45,39 @@ Template Name: Press
 						</article> <!-- end article -->
 						<?php endwhile; endif; ?>
 						<?php wp_reset_postdata(); ?>
+						<br>
+						<br>
+						<div class="press-releases">
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+							<div class="row">
+								<div class="col-sm-12">
+									<header>
+										<h1 class="page-title">Press Releases</h1>
+									</header>
+								</div>
+							</div>
+							<?php
+								global $post;
+								$args = array( 'posts_per_page' => 10, 'category_name' => 'press-releases', 'order' => 'ASC', 'orderby' => 'title' );
+								$myposts = get_posts( $args );
+								foreach( $myposts as $post ) :  setup_postdata($post);
+							?>
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix row' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+								<div class="col-sm-2 text-center">
+									<span class="date"><?php the_field('date'); ?></span>
+								</div>
+								<div class="col-sm-10">
+									<header>
+										<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+									</header>
+								</div>
+							</article> <!-- end article -->
+							<?php endforeach; ?>
+							<?php endwhile; endif; ?>
+						</div>
 					</div>
 				</div>
 			</div>			
-		</div>
-	</div>
-</section>
-<section class="press-releases">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				<div class="row">
-					<div class="col-sm-8 col-sm-offset-3">
-						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-						<div class="row">
-							<div class="col-sm-12">
-								<header>
-									<h1 class="page-title">Press Releases</h1>
-								</header>
-							</div>
-						</div>
-						<?php
-							global $post;
-							$args = array( 'posts_per_page' => 10, 'category_name' => 'press-releases', 'order' => 'ASC', 'orderby' => 'title' );
-							$myposts = get_posts( $args );
-							foreach( $myposts as $post ) :  setup_postdata($post);
-						?>
-						<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix row' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-							<div class="col-sm-2 text-center">
-								<span class="date"><?php the_field('date'); ?></span>
-							</div>
-							<div class="col-sm-10">
-								<header>
-									<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-								</header>
-							</div>
-						</article> <!-- end article -->
-						<?php endforeach; ?>
-						<?php endwhile; endif; ?>
-					</div>
-				</div>
-			</div>
 		</div>
 	</div>
 </section>
