2166857a7cf1fec5475ddd5a0e367087ff5129d5
andywarthol <andywarthol@gmail.com>
2015-03-06 17:54:27 -0800

    Contact form scaffolding change

--- contact.php
+++ contact.php
@@ -11,10 +11,7 @@ Template Name: Contact
 		<div class="row">
 			<div class="col-sm-12">
 				<div class="row">
-					<div class="col-sm-3">
-						<?php get_sidebar(); ?>
-					</div>
-					<div class="col-sm-9">
+					<div class="col-sm-12 col-md-8 col-md-offset-2">
 						<h1 class="page-title">Contact Us</h1>
 						<div class="row">
 							<div class="col-sm-6 form">
