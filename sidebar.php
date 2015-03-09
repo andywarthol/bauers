51b7b9f547500fd50cb5f721c54f819ca5ae25f4
andywarthol <andywarthol@gmail.com>
2015-03-09 14:55:51 -0700

    Single post

--- sidebar.php
+++ sidebar.php
@@ -6,16 +6,16 @@
 <?php else : ?>
 	
 	<div class="widget about">
-		<?php if ( is_page (array( 225, 181 )) ) {
-			footer_bucket_1();
-		} elseif ( $post->post_parent == 225 ) {
-			footer_bucket_1();
-		} elseif ( is_page( 27 ) || is_page( 90 ) ) {
-			footer_bucket_2();
-		} elseif ( $post->post_parent == 27 || $post->post_parent == 90 ) {
-			footer_bucket_2();
-		} else {
-			//get_sidebar();
+		<?php if ( is_page (array( 225, 181 )) ) { ?>
+			<div class="widget about"><?php footer_bucket_1(); ?></div>
+		<?php } elseif ( $post->post_parent == 225 ) { ?>
+			<div class="widget about"><?php footer_bucket_1(); ?></div>
+		<?php } elseif ( is_page( 27 ) || is_page( 90 ) ) { ?>
+			<div class="widget about"><?php footer_bucket_2(); ?></div>
+		<?php } elseif ( $post->post_parent == 27 || $post->post_parent == 90 ) { ?>
+			<div class="widget about"><?php footer_bucket_2(); ?></div>
+		<?php } else { ?>
+			
 		}?>
 	</div>
 	<div class="widget quick-quote">
