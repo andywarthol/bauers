32e10cbfadec3203a1c4b2002d2ced8fd26603ce
andywarthol <andywarthol@gmail.com>
2015-03-09 13:38:03 -0700

    About page id fix

--- sidebar.php
+++ sidebar.php
@@ -8,7 +8,7 @@
 	<div class="widget about">
 		<?php if ( is_page( 225 ) ) {
 			footer_bucket_1();
-		} elseif ( is_page( 27,90 ) ) {
+		} elseif ( is_page( 27 ) || is_page( 90 ) ) {
 			footer_bucket_2();
 		} elseif ($post->post_parent) {
 			get_sidebar($post->post_parent);
