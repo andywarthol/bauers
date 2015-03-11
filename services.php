da693877763d90232602ab067066812b696a14e2
andywarthol <andywarthol@gmail.com>
2015-03-11 10:49:33 -0500

    Services hero

--- services.php
+++ services.php
@@ -5,8 +5,16 @@ Template Name: Services
 ?>
 
 <?php get_header(); ?>
-
-<section class="services-hero">
+<section class="microhero light" style="background-image: url('<?php the_field('background_image'); ?>');">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-10 col-lg-offset-1">
+				<h2 class="text-center">Don't just get there, make <em>IT</em> an experience.</h2>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="standard">
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12">
@@ -16,140 +24,35 @@ Template Name: Services
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-lg-10 col-lg-offset-1">
-	    	<div class="row">
-	    		<div class="col-sm-12 text-center">
-	    			<h1>Features</h1>
-						<p>Build the optimal rate plans for your product or service, without constraints quickly and easily.</p>
-	    			<a href="../sign-up" class="btn btn-lg btn-primary">Create your free account &raquo;</a>
-	    		</div>
-	    		<div class="col-sm-6">
-	    			<img src="<?php bloginfo('template_directory'); ?>/library/images/macbook-air-chart.png" alt="">
-	    		</div>
-	    	</div>
-	    </div>
-	  </div>
-	</div>
-</section>
-<section class="charters">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-10 col-lg-offset-1 col-md-12">
-	    	<div class="row">
-	    		<div class="col-sm-12">
-	    			<h1>Easy Rate Plan and User Management</h1>
-	    			<p>Intuitive management tools via our user portal, or programmatically using the BillForward API.</p>
-	    		</div>
-	    		<div class="col-sm-3 col-lg-offset-1 iphone col-sm-offset-2"><img src="<?php bloginfo('template_directory'); ?>/library/images/iphone-5c.png" alt=""></div>
-	    		<div class="col-md-6">
-	    			<ul class="features">
-	    				<li>Upgrades / downgrades</li>
-	    				<li>Automatic proration</li>
-	    				<li>Rate plan switching version control</li>
-	    				<li>Dunning management</li>
-	    				<li>User self-service tools</li>
-	    				<li>Multi subscription</li>
-	    			</ul>
-	    			<a href="../sign-up" class="btn btn-lg btn-primary">Create your free account &raquo;</a>
-	    		</div>
-	    	</div>
-	    </div>
-    </div>
-  </div>
-</section>
-<section class="commuters">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-10 col-lg-offset-1 col-md-12 col-sm-12">
-				<h1>Metrics &amp; <br class="visible-xs"/>Analytical Tools</h1>
-				<p>We've tried to keep things as simple as possible for fellow developers to work with us.</p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg-10 col-lg-offset-1 col-md-12">
-	    	<div class="row">
-	    		<div class="col-md-5 col-md-offset-1 col-sm-6 col-lg-offset-2">
-	    			<ul class="features">
-	    				<li>Subscription analytics</li>
-	    				<li>Customer analytics</li>
-	    				<li>Rate plan analytics</li>
-	    				<li>Analytics &amp; metrics API</li>
-	    				<li>Import 3rd party data</li>
-	    			</ul>
-	    			<a href="../sign-up" class="btn btn-lg btn-primary">Create your free account &raquo;</a>
-	    		</div>
-	    		<div class="col-sm-5 mac"><img src="<?php bloginfo('template_directory'); ?>/library/images/mac-chart.png" alt=""></div>
-	    	</div>
-	    </div>
-		</div>
-	</div>
-</section>
-<section class="event-logistics">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-10 col-lg-offset-1 col-md-12 col-sm-12">
-				<h1>Developer Tools</h1>
-				<p>We have tried to keep things as simple as possible for developers to work with BillForward. <br class="visible-lg">Because life's too short for $*1t#y APIs.</p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg-10 col-lg-offset-1 col-md-12">
-	    	<div class="row">
-	    		<div class="col-sm-5 dev"><img src="<?php bloginfo('template_directory'); ?>/library/images/coming-soon.png" alt=""></div>
-	    		<div class="col-sm-7">
-	    			<ul class="features">
-	    				<li>REST API</li>
-	    				<li>Webhooks API</li>
-	    				<li>Fully customizable checkout process</li>
-	    				<li>Fully customizable subscription events email</li>
-	    				<li>3rd party app connector</li>
-	    			</ul>
-	    			<a href="https://docs.billforward.net" target="_blank" class="btn btn-lg btn-primary">View Documentation &raquo;</a>
-	    		</div>
-	    	</div>
-	    </div>
-		</div>
-	</div>
-</section>
-<section id="faq">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-10 col-lg-offset-1 col-md-12">
-				<h1>FAQ</h1>
-				<?php if(get_field('faq')): ?>
- 
-					<div class="panel-group" id="accordion">
-
-					<?php while(has_sub_field('faq')): ?>
-
-						<div class="panel panel-default">
-					    <div class="panel-heading">
-					      <h4 class="panel-title">
-					        <a data-toggle="collapse" data-parent="#accordion" href="#collapse<?php the_sub_field('id'); ?>">
-					          <?php the_sub_field('question'); ?>
-					        </a>
-					      </h4>
-					    </div>
-					    <div id="collapse<?php the_sub_field('id'); ?>" class="panel-collapse collapse">
-					      <div class="panel-body">
-					        <?php the_sub_field('answer'); ?>
-					      </div>
-					    </div>
-					  </div>
-				
-					<?php endwhile; ?>
-				
+			<div class="col-sm-12">
+				<div class="row">
+					<div class="col-sm-4 col-md-3">
+						<?php
+							get_sidebar();
+						?>
 					</div>
-				
-				<?php endif; ?>
-			</div>
+					<div class="col-sm-8 col-md-9 col-lg-8">
+						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<div class="row">
+							<div class="col-sm-12">
+								<article>
+									<header>
+										<h1 class="page-title"><?php the_title(); ?></h1>
+									</header>
+									<?php the_content(); ?>
+								</article> <!-- end article -->
+							</div>
+						</div>
+						<?php endwhile; endif; ?>
+					</div>
+				</div>
+			</div>			
 		</div>
 	</div>
 </section>
-<script src="<?php bloginfo('template_directory'); ?>/library/js/jquery.inview.min.js"></script>
 <script>
 	jQuery(document).ready(function($){
-		$('#collapse1.panel-collapse').addClass('in');
+		
 	});
 </script>
 <?php get_footer(); ?>
