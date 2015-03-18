9bbab90c58cd3df58bff07e5b9c15e564a19e982
andywarthol <andywarthol@gmail.com>
2015-03-17 17:21:26 -0700

    Phone number meta content

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
 	<meta name="HandheldFriendly" content="True">
 	<meta name="MobileOptimized" content="320">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+	<meta name="format-detection" content="telephone=yes"/>
 
 	<!-- icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) -->
 	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
