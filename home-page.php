83c091be6c9ad62d949c9d77d2b91ee840e6e141
andywarthol <andywarthol@gmail.com>
2015-03-19 11:32:33 -0700

    Home page hero update

--- home-page.php
+++ home-page.php
@@ -12,11 +12,11 @@ Template Name: Home Page
 				<div class="row">
 					<div class="col-sm-6 col-lg-7">
 						<h1>Professional Transportation <br class="hidden-xs"/>for Any Occasion</h1>
-						<!-- <h2>Let our dedicated account managers, advanced technology platform, and extensive security checks ensure you'll have peace of mind. Over 25 years of experience with:</h2> -->
+						<h2>Let our dedicated account managers, advanced technology platform, and extensive security checks ensure you'll have peace of mind. Over 25 years of experience with:</h2>
 						<ul>
-							<li>Charter, corporate, and event transportation</li>
-							<li>25+ years of transportation experience</li>
-							<li>Dedicated account managers support every trip</li>
+							<li>Charters</li>
+							<li>Corporate Commuter Programs</li>
+							<li>Major Event Logistics and Transporation</li>
 						</ul>
 					</div>
 					<div class="col-sm-6 col-lg-5">
