070e2991718378a0bbf170e7394b5f18cdd4896d
andywarthol <andywarthol@gmail.com>
2015-03-20 11:03:07 -0700

    Adding TC and privacy policy links to footer

--- footer.php
+++ footer.php
@@ -25,67 +25,11 @@
 
 				</div> <!-- end #inner-footer -->
 
-				<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>&nbsp;&nbsp;| <a href="#">Terms &amp; Conditions&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Privacy Policy</a></a></p>
+				<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>&nbsp;&nbsp;| <a href="?p=175">Terms &amp; Conditions</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="?p=547">Privacy Policy</a></p>
 
 				</div> <!-- end .container -->
 
 			</footer> <!-- end footer -->
-
-			<!-- Quick Quote Modal -->
-			<div class="modal fade" id="intake">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-body">
-							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-							<form action="">
-								<legend>Get a Quote<small>We'll get back to you in 24 hours</small></legend>
-								<fieldset>
-									<div class="form-group">
-										<select name="service" id="service" class="form-control">
-											<option value="">Which service would you like?</option>
-											<option value="charter">Charter</option>
-											<option value="commuter">Commuter</option>
-											<option value="events">Event Logistics Management</option>
-										</select>
-									</div>
-									<div class="form-group row">
-										<div class="col-sm-6 left">
-											<input type="text" name="first_name" placeholder="First name" class="form-control">
-										</div>
-										<div class="col-sm-6 right">
-											<input type="text" name="last_name" placeholder="Last name" class="form-control">
-										</div>
-									</div>
-									<div class="form-group">
-										<input type="email" class="form-control" name="email_address" placeholder="Email address">
-									</div>
-									<div class="form-group">
-										<input type="tel" class="form-control" name="phone_number" placeholder="Phone number">
-									</div>
-									<div class="form-group">
-										<input type="text" class="form-control" name="company_name" placeholder="Company name">
-									</div>
-									<div class="form-group row">
-										<div class="col-sm-6 left">
-											<input type="text" name="pickup_date" placeholder="Pick up date" class="form-control">
-										</div>
-										<div class="col-sm-6 right">
-											<input type="text" name="passenger_count" placeholder="# of passengers" class="form-control">
-										</div>
-									</div>
-									<div class="form-group">
-										<button class="btn btn-lg btn-primary btn-block">Get a quote &rarr;</button>
-									</div>
-								</fieldset>
-							</form>
-						</div>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-							<button type="button" class="btn btn-primary">Save changes</button>
-						</div>
-					</div><!-- /.modal-content -->
-				</div><!-- /.modal-dialog -->
-			</div><!-- /.modal -->
 
 		</div> <!-- end .wrapper -->
 
