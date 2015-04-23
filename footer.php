7954917283f39f8e90baef4862bc41e2bd72a4e8
andywarthol <andywarthol@gmail.com>
2015-04-23 14:23:19 -0700

    Adding GA if else 404 logic

--- footer.php
+++ footer.php
@@ -34,7 +34,54 @@
 		</div> <!-- end .wrapper -->
 
 		<!-- all js scripts are loaded in library/bones.php -->
+
+		<!-- jQuery UI -->
 		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+		<!-- Adroll 
+		<script type="text/javascript">
+		adroll_adv_id = "PCRCWFCHFNHWJL7M4OHO5M";
+		adroll_pix_id = "P3GPK62QFVBGJJVWA3OVCQ";
+		(function () {
+		var oldonload = window.onload;
+		window.onload = function(){
+		   __adroll_loaded=true;
+		   var scr = document.createElement("script");
+		   var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+		   scr.setAttribute('async', 'true');
+		   scr.type = "text/javascript";
+		   scr.src = host + "/j/roundtrip.js";
+		   ((document.getElementsByTagName('head') || [null])[0] ||
+		    document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+		   if(oldonload){oldonload()}};
+		}());
+		</script> -->
+
+		<!-- DoubleClick
+		<noscript>
+		<div style="display:inline;">
+		<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/994542426/?value=0&amp;guid=ON&amp;script=0"/>
+		</div>
+		</noscript> -->
+
+		<!-- Google Conversion Tracking
+		<script type="text/javascript">
+		/* <![CDATA[ */
+		var google_conversion_id = 994542426;
+		var google_custom_params = window.google_tag_params;
+		var google_remarketing_only = true;
+		/* ]]> */
+		</script>
+		<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+		</script> -->
+
+		<!-- New Relic
+		<script type="text/javascript">if(!NREUMQ.f){NREUMQ.f=function(){NREUMQ.push(["load",new Date().getTime()]);var e=document.createElement("script");e.type="text/javascript";e.src=(("http:"===document.location.protocol)?"http:":"https:")+"//"+"js-agent.newrelic.com/nr-100.js";document.body.appendChild(e);if(NREUMQ.a)NREUMQ.a();};NREUMQ.a=window.onload;window.onload=NREUMQ.f;};NREUMQ.push(["nrfj","beacon-2.newrelic.com","9794408066","2124736","ZQZQNkNWD0UCAhBfClxMZxBYGAhYBwQcGBVaEw==",0,134,new Date().getTime(),"","","","",""]);</script> -->
+
+		<!-- CallRail
+		<script type="text/javascript" src="//cdn.callrail.com/companies/229295903/d3fb626710a255df8c4d/12/swap.js"></script> -->
+
+
 		<?php wp_footer(); ?>
 
 	</body>
--- header.php
+++ header.php
@@ -19,6 +19,32 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
 	<meta name="format-detection" content="telephone=yes"/>
 
+	<!-- Google Site Verification
+	<meta name="google-site-verification" content="m6tcjYmOS1_5royc-uazuKSNNugHrUy3Kub_BfL-nOs"/> -->
+
+	<!-- Google Analytics -->
+	<?php if ( is_404() ) { ?>
+	<script>			
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-25072936-1', 'auto');
+		ga('require', 'displayfeatures');
+		ga('send', 'pageview', '/404.html?page=' + document.location.pathname + document.location.search + '&from=' + document.referrer]);
+	</script>
+	<?php } else { ?>
+	<script>			
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-25072936-1', 'auto');
+		ga('require', 'displayfeatures');
+		ga('send', 'pageview');
+	</script>
+	<?php } ?>
+
 	<!-- icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) -->
 	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
 	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
