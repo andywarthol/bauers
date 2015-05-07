d23c74741cc14fe750704b5a9d6f73cbccd4e8a9
andywarthol <andywarthol@gmail.com>
2015-05-07 16:25:44 -0700

    datepicker not readonly now

--- home-page.php
+++ home-page.php
@@ -56,7 +56,7 @@ Template Name: Home Page
 									</div>
 									<div class="form-group row supplementary">
 										<div class="col-sm-6 left">
-											<input readonly="true" type="text" id="pickup_date" name="00NE0000000cAFG" placeholder="Pick up date" class="form-control" required="" value="">
+											<input type="text" id="pickup_date" name="00NE0000000cAFG" placeholder="Pick up date" class="form-control" required="" value="">
 										</div>
 										<div class="col-sm-6 right">
 											<input type="text" id="passenger_count" name="00NE0000000cE27" placeholder="# of passengers" class="form-control" required="">
--- sidebar.php
+++ sidebar.php
@@ -57,7 +57,7 @@
 					<input type="text" class="form-control" id="company" name="company" placeholder="Company name" required="">
 				</div>
 				<div class="form-group supplementary">
-					<input readonly="true" type="text" id="pickup_date" name="00NE0000000cAFG" placeholder="Pick up date" class="form-control" required="" value="">
+					<input type="text" id="pickup_date" name="00NE0000000cAFG" placeholder="Pick up date" class="form-control" required="" value="">
 				</div>
 				<div class="form-group supplementary">
 					<input type="text" id="passenger_count" name="00NE0000000cE27" placeholder="# of passengers" class="form-control" required="">
