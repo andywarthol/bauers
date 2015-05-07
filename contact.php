140f6dc30248e15e8f39b4726b22d605c06eeabc
andywarthol <andywarthol@gmail.com>
2015-05-07 14:20:04 -0700

    finishing up the contact form

--- contact.php
+++ contact.php
@@ -22,20 +22,13 @@ Template Name: Contact
 									<input type=hidden name="orgid" value="00DE0000000IvUX">
 									<input type=hidden name="retURL" value="<?php bloginfo('url'); ?>/about/contact-thank-you/">
 
-									<!--  ----------------------------------------------------------------------  -->
-									<!--  NOTE: These fields are optional debugging elements. Please uncomment    -->
-									<!--  these lines if you wish to test in debug mode.                          -->
-									<!--  <input type="hidden" name="debug" value=1>                              -->
-									<!--  <input type="hidden" name="debugEmail" value="ckwak@bauersit.com">      -->
-									<!--  ----------------------------------------------------------------------  -->
-
 									<div class="form-group">
 										<label for="name">Contact Name</label>
 										<input class="form-control" id="name" maxlength="80" name="name" size="20" type="text"  required=""/>
 									</div>
 
 									<div class="form-group">
-										<label for="email">Email address</label>
+										<label for="email">Email Address</label>
 										<input class="form-control" id="email" maxlength="80" name="email" size="20" type="text"  required=""/>
 									</div>
 
@@ -46,7 +39,7 @@ Template Name: Contact
 
 									<div class="form-group">
 										<label for="description">Message</label>
-										<textarea class="form-control" name="description" required=""></textarea>
+										<textarea class="form-control" name="description"></textarea>
 									</div>
 
 									<input type="hidden"  id="external" name="external" value="1" />
